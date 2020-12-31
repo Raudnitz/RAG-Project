b20ccc9554dd1cffb80047d6691bf0d2c812e059
guycarmy <guycarmy@gmail.com>
2020-12-31 11:18:57 +0200

    Use box (form material-ui) for each open game

--- rag-frontend/src/App.js
+++ rag-frontend/src/App.js
@@ -1,10 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import OpenGamesList from './OpenGamesList.js';
 function App() {
-  const [openGames, setOpenGames] = useState(['game1', 'game2'])
-  return (
-      <OpenGamesList key={openGames} openGames={openGames} />
-  )
+    const [openGames, setOpenGames] = useState([
+        {
+            id: 'g1',
+            name: 'game1',
+            players: ['p1', 'p2'],
+
+        },
+        {
+            id: 'g1',
+            name: 'game1',
+            players: ['p1', 'p2'],
+
+        }
+    ])
+    const [players, setPlayers] = useState([
+        {
+            id: 'p1',
+            name: 'player1',
+            score: 10
+        },
+        {
+            id: 'p2',
+            name: 'player2',
+            score: 10
+        }
+    ])
+
+    return (
+        <OpenGamesList key={openGames} openGames={openGames} />
+    )
 }
 
-export default App ;
+export default App;
--- rag-frontend/src/OpenGame.js
+++ rag-frontend/src/OpenGame.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Box from '@material-ui/core/Box'
 
 export default function OpenGame({ openGame }) {
     return(
             <div>
-                {openGame}
+                <Box bgcolor='#f1f1f1' m={1} p={1}>
+                    {openGame.name}
+                    <br/>
+                    {openGame.players}
+                    <br/>
+                    <button>Play</button>
+                </Box>
             </div>
     )
 
