8d3ba9a435fdbcc0b6bd316fe54ae47204c703b9
guycarmy <guycarmy@gmail.com>
2020-12-31 17:07:04 +0200

    Adding rag-backend with mocks

---
+++ rag-backend/README.md
@@ -0,0 +1,12 @@
+# RAG Backend
+This service will be the backend of RAG project.
+
+It is written in Python 3.
+## Run
+First install dependencies
+```
+pip install -r requirments.txt
+```
+Then you can run it locally
+```
+python main.py
---
+++ rag-backend/main.py
@@ -0,0 +1,57 @@
+from flask import Flask
+import json
+
+
+api = Flask(__name__)
+
+GAMES_MOCK = """
+[
+    {
+        {
+            id: 'g1',
+            name: 'game1',
+            players: ['p1', 'p2'],
+
+        },
+        {
+            id: 'g2',
+            name: 'game1',
+            players: ['p1', 'p2'],
+
+        }
+
+    }
+]
+"""
+
+PLAYERS_MOCK = """
+[
+    {
+        {
+            id: 'p1',
+            name: 'player1',
+            score: 10
+        },
+        {
+            id: 'p2',
+            name: 'player2',
+            score: 10
+        }
+
+    }
+]
+"""
+
+
+@api.route('/get_games')
+def get_games():
+    return GAMES_MOCK
+
+
+@api.route('/get_players')
+def get_players():
+    return PLAYERS_MOCK
+
+
+if __name__ == '__main__':
+    api.run(port=4000, host='0.0.0.0')
